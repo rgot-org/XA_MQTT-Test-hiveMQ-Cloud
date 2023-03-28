624102e5b7329561c361bb9bfa3ba8593f2792e0
rgot-org <31351194+rgot-org@users.noreply.github.com>
2023-03-28 17:55:00 +0200

    Application utilisant MQTT.net permettant de se conncter au cloud hiveMQ (connexion chifrée) L'application utilise une classe MqttHandler pour gérer la communicationMQTT

---
+++ AppMQTT/AppMQTT/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppMQTT
+{
+    public partial class MainPage : ContentPage
+    {
+        MqttHandler client = new MqttHandler();
+        public MainPage()
+        {
+            InitializeComponent();
+            MessagingCenter.Subscribe<MqttHandler,String>(this, "message", (sender, message) => _majProprietes(message));
+            MessagingCenter.Subscribe<MqttHandler>(this, "connected", (sender) => _majConnexion(sender));
+            Config config = Config.init("config.json");    
+            client.MqttInit(config);
+        }
+
+        private void _majProprietes(String message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Label_MessageRecu.Text = message;
+            });
+        }
+        private void _majConnexion(MqttHandler sender)
+        {
+            if (sender.isConnected())
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    status.Text = "connecté";
+                });
+            }            
+            
+        }
+
+
+
+        /*******************************************************************************/
+        /*                     gestion des événements de l'IHM                         */
+        /*******************************************************************************/
+        private void btn_Publier_Clicked(object sender, EventArgs e)
+        {
+            client.publishMessage(Entry_MessageToSend.Text);
+        }
+
+        
+    }
+}
---
+++ AppMQTT/AppMQTT/MqttHandler.cs
@@ -0,0 +1,109 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppMQTT
+{
+    //class Config
+    //{
+    //    public string Server { get; set; }
+    //    public int Port { get; set; }
+    //    public string User { get; set; }
+    //    public string Password { get; set; }
+    //    public string PubTopic { get; set; }
+    //    public string SubTopic { get; set; }
+    //}
+    class MqttHandler
+    {
+        MqttFactory factory = new MqttFactory();
+        private IMqttClient mqttClient;
+        private Config _config { get; set; }
+        public Boolean isConnected()
+        {
+            return mqttClient.IsConnected;
+        }
+        public void MqttInit(Config config)
+        {
+            _config = config;
+            mqttClient = factory.CreateMqttClient();
+            mqttClient.ConnectedAsync += (e => ConnectedHandler(e));// une fois que le client est connecte appelle la fonction  "ConnectedHandler"
+            mqttClient.ApplicationMessageReceivedAsync += e => MessageReceivedHandler(e);// à la reception d'un message appelle la fonction "MessageReceivedHandler"
+            mqttClient.DisconnectedAsync += e => DisconnectedHandler(e);// à la deconnexion apelle "DisconnectedHandler"
+            Task.Run(async () => { await Connect(); }); // connexion initiale
+            //Connect().Wait();
+            //await Connect();
+        }
+        public async Task Connect()
+        {
+            // voir https://github.com/chkr1011/MQTTnet/wiki/Client#tcp-connection 
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(_config.Server, _config.Port)
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTls(new MqttClientOptionsBuilderTlsParameters() //https://github.com/chkr1011/MQTTnet/issues/1211
+                {
+                    UseTls = true, // Is set by default to true, I guess...
+                    SslProtocol = SslProtocols.Tls12, // TLS downgrade
+                    AllowUntrustedCertificates = true, // Not sure if this is really needed...
+                    IgnoreCertificateChainErrors = true, // Not sure if this is really needed...
+                    IgnoreCertificateRevocationErrors = true, // Not sure if this is really needed...
+                    CertificateValidationHandler = (w) => true // Not sure if this is really needed...
+                })
+                .WithCredentials(_config.User, _config.Password)
+                .WithCleanSession()
+            .Build();
+            try // essaye
+            {
+                // voir https://github.com/chkr1011/MQTTnet/wiki/Client#connecting
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+            }
+            catch (Exception ex) // en cas d'echec
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public async void publishMessage(String value)
+        {
+            // voir https://github.com/chkr1011/MQTTnet/wiki/Client#publishing-messages
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic(_config.PubTopic)
+                .WithPayload(value)
+                .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                .WithRetainFlag()
+                .Build();
+            await mqttClient.PublishAsync(message, CancellationToken.None); // Since 3.0.5 with CancellationToken
+        }
+        /*******************************************************************************/
+        /*                   zone des handler (gestionnaires)                          */
+        /*******************************************************************************/
+        private async Task ConnectedHandler(MqttClientConnectedEventArgs arg)
+        {
+             MessagingCenter.Send(this, "connected");
+            // Subscribe to a topic
+            // voir https://github.com/chkr1011/MQTTnet/wiki/Client#subscribing-to-a-topic
+            await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(_config.SubTopic).Build());
+        }
+        private async Task MessageReceivedHandler(MqttApplicationMessageReceivedEventArgs arg)
+        {
+            // voir https://github.com/chkr1011/MQTTnet/wiki/Client#consuming-messages
+            //recupération le la payload et conversion byte[]-> string
+            String str = Encoding.UTF8.GetString(arg.ApplicationMessage.Payload);
+            MessagingCenter.Send(this,"message", str);
+        }
+
+
+        // à la deconnexion
+        private async Task DisconnectedHandler(MqttClientDisconnectedEventArgs arg)
+        {
+            // voir https://github.com/chkr1011/MQTTnet/wiki/Client#reconnecting
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await Connect();
+        }
+    }
+}
